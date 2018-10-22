e51cc4cdc23c9ea13ef8f747964f0e223dff4790
Swetha Palwai <palvaiswethareddy@gmail.com>
2018-10-22 16:15:58 +0530

    styles changed

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,22 @@ function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-130px";
+    document.getElementById("navbar").style.top = "-245px";
   }
 }
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
+$(function(){
+	$(".scroll").click(function(){
+		$("html,body").animate({scrollTop:$("body").offset().top
+	},"1000");return false})
+});
+$('.dropdown-toggle').click(function(){
+	$('div').removeClass('dropdown-backdrop');
+});
