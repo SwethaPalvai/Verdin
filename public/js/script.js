0b5c5efe9ae7503a2572ebeee1a7e5e585d5066f
Swetha Palwai <palvaiswethareddy@gmail.com>
2018-10-23 17:54:51 +0530

    who we are added

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,4 @@
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.top = "0";
@@ -15,8 +14,22 @@ $(window).scroll(function() {
         $('.scrolltop').stop(true, true).fadeOut();
     }
 });
+
 $(function(){
 	$(".scroll").click(function(){
 		$("html,body").animate({scrollTop:$("body").offset().top
 	},"1000");return false})
+});
+
+$(document).on('click', '.topnava', function(){
+	if($(this).siblings().hasClass('hide'))
+	{
+		$(this).siblings().removeClass('hide');
+		$(this).siblings().addClass('show');
+	}
+	else
+	{
+		$(this).siblings().removeClass('show');
+		$(this).siblings().addClass('hide');
+	}
 });
