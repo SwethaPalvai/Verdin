148d4fe3dfb7222b5738d124d558cc4b27702a56
Swetha Palwai <palvaiswethareddy@gmail.com>
2018-10-27 13:33:10 +0530

    skill set added

--- public/js/scriptnew.js
+++ public/js/scriptnew.js
@@ -1,3 +1,12 @@
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-20em";
+  }
+}
+
 $(window).scroll(function() {
     if ($(this).scrollTop() > 50 ) {
         $('.scrolltop:hidden').stop(true, true).fadeIn();
@@ -11,27 +20,33 @@ $(function(){
 		$("html,body").animate({scrollTop:$("body").offset().top
 	},"1000");return false})
 });
-$(document).ready(function(){
-	if ($(window).width() <= 767) {
-		if($('.navbar-inverse').hasClass('hide')) {
-			$('.navbar-inverse').removeClass('hide');
-			$('.navbar-inverse').addClass('show');
-			$('.navitems').addClass('hide');
-			$('.navbar-inverse').css({"background-color":"white","border-color":"white","border":"none"});
-			$('.navbar-collapse').css({"border-color":"white"});
-		}
+// $(document).ready(function(){
+// 	if ($(window).width() <= 767) {
+// 		if($('.navbar-inverse').hasClass('hide')) {
+// 			$('.navbar-inverse').removeClass('hide');
+// 			$('.navbar-inverse').addClass('show');
+// 			$('.navitems').addClass('hide');
+// 			$('.navbar-inverse').css({"background-color":"white","border-color":"white","border":"none"});
+// 			$('.navbar-collapse').css({"border-color":"white"});
+// 		}
+// 	}
+// });
+
+$(document).on('click', '.navbarright li', function(){
+	if($('.navbarright li').siblings().hasClass('active')) {
+		$('.navbarright li').siblings().removeClass('active');
+		$(this).addClass('active');
 	}
 });
-
-$(document).on('click', '.navitems .navli', function(){
+$(document).on('click', '.navitems .linkli', function(){
 	if($('.navitems li').siblings().hasClass('active')) {
 		$('.navitems li').siblings().removeClass('active');
 		$(this).addClass('active');
 	}
 });
-$(document).on('click', '.dropdown-menu li', function(){
-	if($('.dropdown-menu li').siblings().hasClass('active')) {
-		$('.navitems li').siblings().removeClass('active');
-	}
-	$(this).addClass('active');
-});
+// $(document).on('click', '.dropdown-menu li', function(){
+// 	if($('.dropdown-menu li').siblings().hasClass('active')) {
+// 		$('.navitems li').siblings().removeClass('active');
+// 	}
+// 	$(this).addClass('active');
+// });
